34bba70e10403d10682671c04b8fb84a95c2b35a
Emmeran Colot <emmeran.colot@gmail.com>
2024-06-16 04:42:40 +0200

    added shell scripts for debugging/compilation

---
+++ tests/compile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Vérifie si un argument est passé
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
+
+# Nom du fichier sans l'extension
+FILENAME=$1
+
+# Assemble le fichier .asm en fichier objet .o
+nasm -f elf "$FILENAME.asm"
+if [ $? -ne 0 ]; then
+  echo "Erreur : échec de l'assemblage de $FILENAME.asm"
+  exit 1
+fi
+
+# Enleve l'ancien exécutable
+rm "$FILENAME"
+
+# Lie le fichier objet pour créer l'exécutable
+ld -m elf_i386 -s -o "$FILENAME" "$FILENAME.o"
+if [ $? -ne 0 ]; then
+  echo "Erreur : échec de la liaison de $FILENAME.o"
+  exit 1
+fi
+
+# Supprime le fichier objet intermédiaire
+rm "$FILENAME.o"
+
+# Execute le resultat
+./$FILENAME
